70e99e432eefbf12db835568d2020a088d0fce62
kim-913 <kziruo@gmail.com>
2021-08-01 20:06:02 -0700

    7.29

--- solution_7_29.java
+++ solution_7_29.java
@@ -55,5 +55,26 @@ public class solution_7_29 {
         //     availableJumps = Math.max(availableJumps--, nums[i]);
         // }
         // return true;
+    }    public int countSubstrings(String s) {
+        int res = 0;
+        for(int i = 0; i <= s.length(); i++){
+            for(int j = i+1; j <= s.length(); j++){
+                if(isPalindrome(s.substring(i,j))) res++;
+            }
+        }
+        return res;
     }
+
+    // LeetCode 647. Palindromic Substrings
+    public boolean isPalindrome(String s){
+        int left = 0, right = s.length()-1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
 }
