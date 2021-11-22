6e7b1847c09df2429dea8e2b324bcdead7638dbd
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-11-22 11:58:25 -0800

    45

--- java/review_google_vo.java
+++ java/review_google_vo.java
@@ -1,3 +1,5 @@
+package java;
+
 /**
  * review_google_vo
  */
---
+++ problems/45.java
@@ -0,0 +1,20 @@
+package problems;
+
+public class 45 {
+    
+
+    class Solution {
+        public int jump(int[] nums) {
+            int res = 0, farthest = 0, currentJumpEnd = 0;
+            for(int i = 0; i < nums.length - 1; i++){
+                farthest = Math.max(farthest, i + nums[i]);
+                // if current jump is to the end, need to make another jump
+                if(i == currentJumpEnd) {
+                    res++;
+                    currentJumpEnd = farthest;
+                }
+            }
+            return res;
+        }
+    }
+}
