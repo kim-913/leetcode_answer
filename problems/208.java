9b6effc349b9e98ce5eadd83315f145ddb4d999e
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-11-22 13:05:02 -0800

    Implement Trie (Prefix Tree)

--- problems/208.java
+++ problems/208.java
@@ -1,60 +1,55 @@
 package problems;
 
-public class 208 {
-    
-
-    class Trie {
-        class TrieNode {
-            boolean isEnd;
-            TrieNode[] children;
-            public TrieNode (){
-                children = new TrieNode[26];
-                isEnd = false;
-            }
-        }
-        private TrieNode root;
-        public Trie() {
-            root = new TrieNode();
-        }
-        
-        public void insert(String word) {
-            TrieNode p = root;
-            for(int i = 0; i < word.length(); i++){
-                char c = word.charAt(i);
-                int index = c - 'a';
-                if(p.children[index] == null) p.children[index] = new TrieNode();
-                p = p.children[index];
-            }
-            p.isEnd = true;
-        }
-        
-        public boolean search(String word) {
-            TrieNode node = find(word);
-            return node != null && node.isEnd;
-        }
-        
-        public boolean startsWith(String prefix) {
-            return find(prefix) != null;
-        }
-        
-        public TrieNode find(String word){
-            TrieNode p = root;
-            for(int i = 0; i < word.length(); i++){
-                char c = word.charAt(i);
-                int index = c - 'a';
-                if(p.children[index] == null) return null;
-                p = p.children[index];
-            }
-            return p;
+class Trie {
+    class TrieNode {
+        boolean isEnd;
+        TrieNode[] children;
+        public TrieNode (){
+            children = new TrieNode[26];
+            isEnd = false;
         }
     }
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
     
-    /**
-     * Your Trie object will be instantiated and called as such:
-     * Trie obj = new Trie();
-     * obj.insert(word);
-     * boolean param_2 = obj.search(word);
-     * boolean param_3 = obj.startsWith(prefix);
-     */
+    public void insert(String word) {
+        TrieNode p = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if(p.children[index] == null) p.children[index] = new TrieNode();
+            p = p.children[index];
+        }
+        p.isEnd = true;
+    }
     
+    public boolean search(String word) {
+        TrieNode node = find(word);
+        return node != null && node.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        return find(prefix) != null;
+    }
+    
+    public TrieNode find(String word){
+        TrieNode p = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if(p.children[index] == null) return null;
+            p = p.children[index];
+        }
+        return p;
+    }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
