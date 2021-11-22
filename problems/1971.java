f221bb1847520177cf68b47a4f40fefba877533d
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-11-22 13:37:32 -0800

    Find if Path Exists in Graph

---
+++ problems/1971.java
@@ -0,0 +1,49 @@
+package problems;
+
+import java.util.*;
+
+class Solution {
+    // boolean[] visited;
+    // public boolean validPath(int n, int[][] edges, int start, int end) {
+    //     Map<Integer, List<Integer>> buildGraph = new HashMap<>();
+    //     visited = new boolean[n];
+    //     for(int i = 0; i < n; i++){
+    //         buildGraph.put(i, new ArrayList<>());
+    //     }
+    //     for(int[] edge: edges){
+    //         buildGraph.get(edge[0]).add(edge[1]);
+    //         buildGraph.get(edge[1]).add(edge[0]);
+    //     }
+    //     return dfs(buildGraph, start, end, start);
+    // }
+    
+    // public boolean dfs(Map<Integer, List<Integer>> graph, int start, int end, int cur){
+    //     if(cur == end) return true;
+    //     visited[cur] = true;
+    //     List<Integer> neighbors = graph.get(cur);
+    //     for(int neighbor: neighbors){
+    //         if(!visited[neighbor]) {
+    //             return dfs(graph, start, end, neighbor);
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    public int[] parent;
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        parent = new int[n];
+        for(int i = 0; i < n; i++) parent[i] = i;
+        for(int[] edge : edges) {
+            int p1 = findParent(edge[0]);
+            int p2 = findParent(edge[1]);
+            if(p1 != p2) {
+                parent[p1] = p2;
+            }
+        }
+        return findParent(start) == findParent(end);
+    }
+
+    private int findParent(int child) {
+        return parent[child] == child ? child : (parent[child] = findParent(parent[child]));
+    }
+}
