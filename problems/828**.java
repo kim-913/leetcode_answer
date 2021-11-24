0a917e193fc5d3f304930fb6e2e3af8fbd28f305
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-11-23 18:22:46 -0800

    Count Unique Characters of All Substrings of a Given String

---
+++ problems/828**.java
@@ -0,0 +1,19 @@
+package problems;
+class Solution {
+    // https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/discuss/128952/C%2B%2BJavaPython-One-pass-O(N)
+
+    // solution is too smart to come out
+    public int uniqueLetterString(String s) {
+        int[][] index = new int[26][2];
+        for (int i = 0; i < 26; ++i) Arrays.fill(index[i], -1);
+        int res = 0, N = s.length(), mod = (int)Math.pow(10, 9) + 7;
+        for (int i = 0; i < N; ++i) {
+            int c = s.charAt(i) - 'A';
+            res = (res + (i - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+            index[c] = new int[] {index[c][1], i};
+        }
+        for (int c = 0; c < 26; ++c)
+            res = (res + (N - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+        return res;
+    }
+}
