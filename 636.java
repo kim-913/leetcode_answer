edbf592bbe4f4c86bc80381e1fcabd3d760ac24a
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-12-01 14:49:53 -0800

    Exclusive Time of Functions

---
+++ 636.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Deque<Log> stack = new ArrayDeque<>();
+        int[] result = new int[n];
+        for (String content : logs) {
+            Log log = new Log(content);
+            if (log.isStart) {
+                stack.push(log);
+            } else {
+                Log top = stack.pop();
+                result[top.id] += (log.time - top.time + 1);
+                if (!stack.isEmpty()) {
+                    result[stack.peek().id] -= (log.time - top.time + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static class Log {
+        public int id;
+        public boolean isStart;
+        public int time;
+
+        public Log(String input) {
+            String[] logs = input.split(":");
+            id = Integer.valueOf(logs[0]);
+            isStart = logs[1].equals("start");
+            time = Integer.valueOf(logs[2]);
+        }
+    }
+}
