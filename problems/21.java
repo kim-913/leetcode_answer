658f80d3ea5e175a0e446a6be7ad7a3250b3b73e
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-12-02 15:08:26 -0800

    Merge Two Sorted Lists

--- problems/21.java
+++ problems/21.java
@@ -37,3 +37,26 @@ class Solution {
         return res.next;
     }
 }
+
+// solution3 - bf - easy to understand
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode tail = ans;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 != null)
+            tail.next = l1;
+        if (l2 != null)
+            tail.next = l2;
+        return ans.next;
+    }
+}
