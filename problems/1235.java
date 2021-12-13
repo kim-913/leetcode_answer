defe69f068068a4c5806124383d18c263973f2c6
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-12-13 11:34:40 -0800

    Maximum Profit in Job Scheduling

--- problems/1235.java
+++ problems/1235.java
@@ -8,6 +8,9 @@ class Solution {
         TreeMap<Integer, Integer> dp = new TreeMap<>();
         dp.put(0, 0);
         for (int[] job : jobs) {
+            // The floorEntry(K key) method is used to return a key-value mapping associated
+            // with the greatest key less than or equal to the given key, or null if there
+            // is no such key.
             int cur = dp.floorEntry(job[0]).getValue() + job[2];
             // System.out.println(dp.floorEntry(job[0]));
             if (cur > dp.lastEntry().getValue())
