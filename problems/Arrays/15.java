6cf0ff40cd4d63f811033c7c1bd23b9b1d0dffff
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2021-12-17 16:07:14 -0800

    3Sum

---
+++ problems/Arrays/15.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0)
+            return res;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1, right = nums.length - 1;
+                while (left < right) {
+                    if (nums[left] + nums[right] + nums[i] == 0) {
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--;
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] + nums[i] < 0)
+                        left++;
+                    else
+                        right--;
+                }
+            }
+        }
+        return res;
+    }
+}
