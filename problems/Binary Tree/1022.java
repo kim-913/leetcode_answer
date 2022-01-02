daf588775e1e012070cdad39b2cf82fba6555625
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2022-01-02 14:30:52 -0800

    Sum of Root To Leaf Binary Numbers

---
+++ problems/Binary Tree/1022.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int val) {
+        if (root == null)
+            return 0;
+        val = val * 2 + root.val;
+        return root.left == root.right ? val : dfs(root.left, val) + dfs(root.right, val);
+    }
+}
