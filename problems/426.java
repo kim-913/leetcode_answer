9b7b277f45b0839b2a4d2c6f8138c47e7f0b759e
Ziruo Ke <ziruoke@Ziruos-MacBook-Pro.local>
2022-01-02 12:25:49 -0800

    Convert Binary Search Tree to Sorted Doubly Linked List

--- problems/426.java
+++ problems/426.java
@@ -1,45 +1,44 @@
-package problems;
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
-
-// In order
 class Solution {
+    // double linkedlist, tail connect again to head
     Node head = null;
     Node tail = null;
+
     public Node treeToDoublyList(Node root) {
-        if(root == null) return root;
+        if (root == null)
+            return root;
         dfs(root);
         tail.right = head;
         head.left = tail;
         return head;
     }
-    
-    private void dfs(Node node){
-        if(node == null) return;
-        dfs(node.left);
-        if(tail != null) {
-            tail.right = node;
-            node.left = tail;
+
+    private Node dfs(Node root) {
+        if (root == null)
+            return null;
+        dfs(root.left);
+        if (tail != null) {
+            tail.right = root;
+            root.left = tail;
         } else {
-            head = node;
+            head = root;
         }
-        tail = node;
-        dfs(node.right);
+        tail = root;
+        /*
+         * 1
+         * 1
+         * 1
+         * 2
+         * 1
+         * 3
+         * 1
+         * 4
+         * 1
+         * 5
+         * Output: [1,2,3,4,5]
+         */
+        // System.out.println(head.val);
+        // System.out.println(tail.val);
+        dfs(root.right);
+        return head;
     }
 }
